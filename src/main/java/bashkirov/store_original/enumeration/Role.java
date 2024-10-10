6d4015040712c43f1623ca717d989396815dd123
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-10-10 13:57:16 +0300

    Clean master branch

--- src/main/java/bashkirov/store_original/enumeration/Role.java
+++
@@ -1,7 +0,0 @@
-package bashkirov.store_original.enumeration;
-
-public enum Role {
-    ADMIN_ROLE,
-    MANAGER_ROLE,
-    USER_ROLE
-}
--- src/main/java/bashkirov/store_original/exception/InvalidActivationKeyException.java
+++
@@ -1,7 +0,0 @@
-package bashkirov.store_original.exception;
-
-public class InvalidActivationKeyException extends RuntimeException {
-    public InvalidActivationKeyException(String message) {
-        super(message);
-    }
-}
